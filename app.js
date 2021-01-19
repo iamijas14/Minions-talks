15c5d42dc8a82ec0ec9639d0131132765e716263
Ijas mohammed <ijas.mi2714@gmail.com>
2021-01-19 22:44:39 +0530

    feat:updated
    translation app completed

---
+++ app.js
@@ -0,0 +1,31 @@
+var buttonTrans = document.querySelector(" #btn-trans ");
+var textInput = document.querySelector("#txt-area")
+var outPut=document.querySelector("#output")
+console.log(buttonTrans);
+
+buttonTrans.addEventListener("click", clickHandler)
+
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
+
+// var serverUrl="https://api.funtranslations.com/translate/dothraki.json"
+
+function getUrl(text)
+{
+    return serverUrl + "?" + "text=" +text
+}
+
+function clickHandler(){
+
+
+    // console.log(textInput.value); 
+    // outPut.innerText=textInput.value;
+
+
+    var inputTxt= textInput.value;
+    fetch(getUrl(inputTxt))
+    .then(response=>response.json())
+    .then(json=>{
+    var translatedText = json.contents.translated;
+    outPut.innerText=translatedText;
+    })
+}
